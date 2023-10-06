937cfd2ff0840e9282e000027e4245aa50d162b7
safinxr <safinxr@gmail.com>
2023-10-06 22:17:35 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        "first-color":"#19CFFC",
+        "second-color": "#12ECD3",
+      }
+    },
+  },
+  plugins: [require("daisyui")],
+}
+
