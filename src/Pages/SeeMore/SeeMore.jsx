9d5260c1f750bd4664552e446f94bc5c0f1fcc13
safinxr <safinxr@gmail.com>
2023-10-07 19:41:07 +0600

    see more page done

--- src/Pages/SeeMore/SeeMore.jsx
+++ src/Pages/SeeMore/SeeMore.jsx
@@ -7,20 +7,20 @@ const SeeMore = () => {
     const findData = data.find(singleData => singleData.id === cardid);
     const { id, services, image, price, description } = findData;
     return (
-        <div className='max-w-7xl mx-auto my-16' >
-            <div className='flex flex-col lg:flex-row'>
+        <div className='max-w-7xl mx-auto px-3 md:px-7 lg:px-0 my-8 lg:my-16' >
+            <div className='flex flex-col lg:flex-row '>
                 <div>
                     <img className='rounded-xl w-full' src={image} alt="" />
                 </div>
                 <div className='flex-1 p-4'>
-                    <h2 className='text-6xl font-black '>{services}</h2>
-                    <div className="flex justify-between items-center py-10">
-                        <p className='text-2xl font-bold text-gradient'>Price: {price}</p>
-                        <Link className="text-xl border border-[#04DDF1] px-4 py-1.5 rounded-md text-gradient uppercase hover:text-black">More details</Link>
+                    <h2 className='text-3xl md:text-5xl font-black uppercase mt-4 md:mt-6 lg:mt-0'>{services}</h2>
+                    <div className="flex justify-between items-center py-4 md:py-6 lg:py-10">
+                        <p className='text-xl  md:text-2xl font-bold text-gradient'>Price: {price}</p>
+                        <Link className="text-base md:text-xl border border-[#04DDF1] px-4 py-1.5 rounded-md text-gradient uppercase hover:text-black">More details</Link>
                     </div>
                 </div>
             </div>
-            <p className='text-xl my-10'>{description}</p>
+            <p className='text-base md:text-xl my-4 md:my-6 lg:my-10'>{description}</p>
         </div>
     );
 };
